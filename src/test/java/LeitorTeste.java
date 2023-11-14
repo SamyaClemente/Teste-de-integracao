bb0eb1ea7d937abed5bfd802ec50b029672ee796
ayshamariaa <115497615+ayshamariaa@users.noreply.github.com>
2023-11-14 01:21:24 -0300

    Update LeitorTeste.java

--- src/test/java/LeitorTeste.java
+++ src/test/java/LeitorTeste.java
@@ -1,3 +1,27 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class LeitorTeste {
-    
+
+    @Test
+    public void testEmprestimoEDevolucao() {
+        Leitor leitor = new Leitor("João", "123456789");
+        
+        Livro livro1 = new Livro("Aventuras no Espaço");
+        Livro livro2 = new Livro("História Antiga");
+
+        leitor.realizarEmprestimo(livro1);
+
+        assertTrue(leitor.getLivrosEmprestados().contains(livro1));
+
+        Livro livroNaoEmprestado = new Livro("Livro Não Emprestado");
+        leitor.devolverLivro(livroNaoEmprestado);
+
+        assertTrue(leitor.getLivrosEmprestados().contains(livro1));
+
+        leitor.devolverLivro(livro1);
+
+        assertFalse(leitor.getLivrosEmprestados().contains(livro1));
+    }
 }
+
